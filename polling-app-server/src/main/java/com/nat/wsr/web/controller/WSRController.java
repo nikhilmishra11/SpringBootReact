126e782c995cbc64f39f24153f48deb9578996c7
tarun.leekha <tarunleekha1@gmail.com>
2018-10-26 15:48:26 +0530

    Created WSRController and did updates for REST URL's

--- polling-app-server/src/main/java/com/nat/wsr/web/controller/WSRController.java
+++ polling-app-server/src/main/java/com/nat/wsr/web/controller/WSRController.java
@@ -1,59 +1,59 @@
-package com.nik.wsr.web.controller;
+package com.nat.wsr.web.controller;
+
+import java.net.URI;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.nik.wsr.model.*;
-import com.nik.wsr.payload.*;
-import com.nik.wsr.security.CurrentUser;
-import com.nik.wsr.security.UserPrincipal;
-import com.nik.wsr.util.AppConstants;
-import com.nik.wsr.web.repository.PollRepository;
-import com.nik.wsr.web.repository.UserRepository;
-import com.nik.wsr.web.repository.VoteRepository;
-import com.nik.wsr.web.service.PollService;
-
-import javax.validation.Valid;
-import java.net.URI;
+import com.nat.wsr.model.Poll;
+import com.nat.wsr.payload.ApiResponse;
+import com.nat.wsr.payload.PagedResponse;
+import com.nat.wsr.payload.PollRequest;
+import com.nat.wsr.payload.PollResponse;
+import com.nat.wsr.security.CurrentUser;
+import com.nat.wsr.security.UserPrincipal;
+import com.nat.wsr.util.AppConstants;
+import com.nat.wsr.web.service.WsrAutomatorService;
 
 /**
- * Created by rajeevkumarsingh on 20/11/17.
+ * 
+ * @author tarun.leekha
+ *
  */
-
 @RestController
-@RequestMapping("/api/polls")
+@RequestMapping("/api/Tasks")
 public class WSRController {
 
-    @Autowired
-    private PollRepository pollRepository;
-
-    @Autowired
-    private VoteRepository voteRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PollService pollService;
-
+	@Autowired
+	private WsrAutomatorService wsrAutomatorService;
+	
     private static final Logger logger = LoggerFactory.getLogger(WSRController.class);
 
     @GetMapping
-    public PagedResponse<PollResponse> getPolls(@CurrentUser UserPrincipal currentUser,
+    public PagedResponse<PollResponse> getTasks(@CurrentUser UserPrincipal currentUser,
                                                 @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
                                                 @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
-        return pollService.getAllPolls(currentUser, page, size);
+    	logger.info("WSRController.getTasks()");
+        return wsrAutomatorService.getAllTask(currentUser, page, size);
     }
 
     @PostMapping
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> createPoll(@Valid @RequestBody PollRequest pollRequest) {
-        Poll poll = pollService.createPoll(pollRequest);
+    public ResponseEntity<?> createTask(@Valid @RequestBody PollRequest pollRequest) {
+        Poll poll = wsrAutomatorService.createTask(pollRequest);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{pollId}")
@@ -64,18 +64,11 @@ public class WSRController {
     }
 
 
-    @GetMapping("/{pollId}")
+    @GetMapping("/{taskId}")
     public PollResponse getPollById(@CurrentUser UserPrincipal currentUser,
-                                    @PathVariable Long pollId) {
-        return pollService.getPollById(pollId, currentUser);
+                                    @PathVariable Long taskId) {
+        return wsrAutomatorService.getTaskById(taskId, currentUser);
     }
 
-    @PostMapping("/{pollId}/votes")
-    @PreAuthorize("hasRole('USER')")
-    public PollResponse castVote(@CurrentUser UserPrincipal currentUser,
-                         @PathVariable Long pollId,
-                         @Valid @RequestBody VoteRequest voteRequest) {
-        return pollService.castVoteAndGetUpdatedPoll(pollId, voteRequest, currentUser);
-    }
 
 }
